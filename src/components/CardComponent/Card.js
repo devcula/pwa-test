220efbc60c3936ed1e7619c2a25c6fbb17226c01
dEvCuLa <abhimaxfield@gmail.com>
2019-06-17 07:56:04 +0530

    Manifest changes for better pwa score

--- src/components/CardComponent/Card.js
+++ src/components/CardComponent/Card.js
@@ -4,7 +4,7 @@ const Card = ({ id, name, email}) => {
     //const { id, name, email} = props;      //Used Destructuring directly above
     return(
         <div className="tc bg-light-green br3 ma2 grow dib pa3 bw2 shadow-5">
-            <img src={`https://robohash.org/${id}?200x200`} alt="robo"/>
+            <img src={`https://robohash.org/${id}?size=200x200`} alt="robo"/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
--- src/components/SearchBoxComponent/SearchBox.js
+++ src/components/SearchBoxComponent/SearchBox.js
@@ -4,6 +4,7 @@ import './SearchBox.css';
 const SearchBox = ({filterFunction}) => {
     return (
         <input 
+        aria-label="Search Bots" 
         className="sbox" 
         type="search" 
         placeholder="Search Bots"

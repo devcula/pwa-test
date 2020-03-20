f8200e7eb447856b3bc52a732b17fc990f232bb8
Abhishek Prasad <devcula@outlook.com>
2020-03-20 12:31:35 +0530

    Updates

---
+++ precache-manifest.982fb570d44232e3f422176bdedaaada.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "858e70e025d9d7b168a76c01d8797f43",
+    "url": "/pwa-test/index.html"
+  },
+  {
+    "revision": "9bae16f687bca4d8604b",
+    "url": "/pwa-test/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "55c76f44e3e05832e6de",
+    "url": "/pwa-test/static/css/main.d4b790a2.chunk.css"
+  },
+  {
+    "revision": "9bae16f687bca4d8604b",
+    "url": "/pwa-test/static/js/2.a7657f60.chunk.js"
+  },
+  {
+    "revision": "dbe5ca7a82c728776bb536c3d2d64135",
+    "url": "/pwa-test/static/js/2.a7657f60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55c76f44e3e05832e6de",
+    "url": "/pwa-test/static/js/main.ce07bdb9.chunk.js"
+  },
+  {
+    "revision": "f9d619f6c82a562443b8",
+    "url": "/pwa-test/static/js/runtime-main.ce20142c.js"
+  }
+]);
